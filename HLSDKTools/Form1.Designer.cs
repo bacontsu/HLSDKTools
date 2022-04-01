bc87d63f6f9ea0e32334ddd377206f98421d745c
Bacontsu <62763382+bacontsu@users.noreply.github.com>
2022-04-01 10:59:28 +0700

    first commit

---
+++ HLSDKTools/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace HLSDKTools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.title = new System.Windows.Forms.Label();
+            this.logo = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.FlatAppearance.BorderSize = 0;
+            this.closeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closeBtn.ForeColor = System.Drawing.Color.Gray;
+            this.closeBtn.Location = new System.Drawing.Point(756, -3);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(47, 51);
+            this.closeBtn.TabIndex = 0;
+            this.closeBtn.Text = "X";
+            this.closeBtn.UseVisualStyleBackColor = true;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            this.closeBtn.MouseEnter += new System.EventHandler(this.closeBtn_MouseEnter);
+            this.closeBtn.MouseLeave += new System.EventHandler(this.closeBtn_MouseLeave);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.panel1.Controls.Add(this.title);
+            this.panel1.Controls.Add(this.logo);
+            this.panel1.Controls.Add(this.closeBtn);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(823, 43);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.White;
+            this.title.Location = new System.Drawing.Point(37, 12);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(97, 19);
+            this.title.TabIndex = 2;
+            this.title.Text = "HLSDK Tools";
+            this.title.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // logo
+            // 
+            this.logo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("logo.BackgroundImage")));
+            this.logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.logo.FlatAppearance.BorderSize = 0;
+            this.logo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.logo.Location = new System.Drawing.Point(8, 6);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(30, 30);
+            this.logo.TabIndex = 1;
+            this.logo.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(0, 43);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(179, 416);
+            this.panel2.TabIndex = 2;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(90)))), ((int)(((byte)(90)))));
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(0, 155);
+            this.button5.Name = "button5";
+            this.button5.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.button5.Size = new System.Drawing.Size(179, 37);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Map Compiler";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.button5.MouseEnter += new System.EventHandler(this.button5_MouseEnter);
+            this.button5.MouseLeave += new System.EventHandler(this.button5_MouseLeave);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(90)))), ((int)(((byte)(90)))));
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(0, 116);
+            this.button4.Name = "button4";
+            this.button4.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.button4.Size = new System.Drawing.Size(179, 37);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Spritemaker";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            this.button4.MouseLeave += new System.EventHandler(this.button4_MouseLeave);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(90)))), ((int)(((byte)(90)))));
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(0, 77);
+            this.button3.Name = "button3";
+            this.button3.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.button3.Size = new System.Drawing.Size(179, 37);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Wadmaker";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.MouseEnter += new System.EventHandler(this.button3_MouseEnter);
+            this.button3.MouseLeave += new System.EventHandler(this.button3_MouseLeave);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(90)))), ((int)(((byte)(90)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(0, 38);
+            this.button2.Name = "button2";
+            this.button2.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.button2.Size = new System.Drawing.Size(179, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "StudioMDL";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseEnter += new System.EventHandler(this.button2_MouseEnter);
+            this.button2.MouseLeave += new System.EventHandler(this.button2_MouseLeave);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(120)))), ((int)(((byte)(120)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(0, -1);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(179, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Model Decompiler";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(59)))), ((int)(((byte)(59)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Button logo;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ HLSDKTools/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace HLSDKTools
+{
+    public partial class Form1 : Form
+    {
+        int button1Offset;
+        int button2Offset;
+        int button3Offset;
+        int button4Offset;
+        int button5Offset;
+
+        int button1State;
+        int button2State;
+        int button3State;
+        int button4State;
+        int button5State;
+        
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr one, int two, int three, int four);
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+
+            // default values
+            button1State = 30;
+            button2State = 30;
+            button3State = 30;
+            button4State = 30;
+            button5State = 30;
+
+            button1Offset = button1State;
+            button2Offset = button2State;
+            button3Offset = button3State;
+            button4Offset = button4State;
+            button5Offset = button5State;
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void closeBtn_MouseEnter(object sender, EventArgs e)
+        {
+            closeBtn.ForeColor = Color.Red;
+        }
+
+        private void closeBtn_MouseLeave(object sender, EventArgs e)
+        {
+            closeBtn.ForeColor = Color.Gray;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(Handle, 0x112, 0xf012, 0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // handle button1
+            if (button1Offset > button1State) button1State++;
+            else if (button1Offset < button1State) button1State--;
+
+            // handle button2
+            if (button2Offset > button2State) button2State++;
+            else if (button2Offset < button2State) button2State--;
+
+            // handle button3
+            if (button3Offset > button3State) button3State++;
+            else if (button3Offset < button3State) button3State--;
+
+            // handle button4
+            if (button4Offset > button4State) button4State++;
+            else if (button4Offset < button4State) button4State--;
+
+            // handle button5
+            if (button5Offset > button5State) button5State++;
+            else if (button5Offset < button5State) button5State--;
+
+            // apply padding
+            button1.Padding = new Padding(button1State, 0, 0, 0);
+            button2.Padding = new Padding(button2State, 0, 0, 0);
+            button3.Padding = new Padding(button3State, 0, 0, 0);
+            button4.Padding = new Padding(button4State, 0, 0, 0);
+            button5.Padding = new Padding(button5State, 0, 0, 0);
+
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1Offset = 40;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1Offset = 30;
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            button2Offset = 40;
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            button2Offset = 30;
+        }
+
+        private void button3_MouseEnter(object sender, EventArgs e)
+        {
+            button3Offset = 40;
+        }
+
+        private void button3_MouseLeave(object sender, EventArgs e)
+        {
+            button3Offset = 30;
+        }
+
+        private void button4_MouseEnter(object sender, EventArgs e)
+        {
+            button4Offset = 40;
+        }
+
+        private void button4_MouseLeave(object sender, EventArgs e)
+        {
+            button4Offset = 30;
+        }
+
+        private void button5_MouseEnter(object sender, EventArgs e)
+        {
+            button5Offset = 40;
+        }
+
+        private void button5_MouseLeave(object sender, EventArgs e)
+        {
+            button5Offset = 30;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // set active color
+            button1.BackColor = Color.FromArgb(120, 120, 120);
+            button2.BackColor = Color.FromArgb(90, 90, 90);
+            button3.BackColor = Color.FromArgb(90, 90, 90);
+            button4.BackColor = Color.FromArgb(90, 90, 90);
+            button5.BackColor = Color.FromArgb(90, 90, 90);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // set active color
+            button2.BackColor = Color.FromArgb(120, 120, 120);
+            button1.BackColor = Color.FromArgb(90, 90, 90);
+            button3.BackColor = Color.FromArgb(90, 90, 90);
+            button4.BackColor = Color.FromArgb(90, 90, 90);
+            button5.BackColor = Color.FromArgb(90, 90, 90);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // set active color
+            button3.BackColor = Color.FromArgb(120, 120, 120);
+            button2.BackColor = Color.FromArgb(90, 90, 90);
+            button1.BackColor = Color.FromArgb(90, 90, 90);
+            button4.BackColor = Color.FromArgb(90, 90, 90);
+            button5.BackColor = Color.FromArgb(90, 90, 90);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // set active color
+            button4.BackColor = Color.FromArgb(120, 120, 120);
+            button2.BackColor = Color.FromArgb(90, 90, 90);
+            button3.BackColor = Color.FromArgb(90, 90, 90);
+            button1.BackColor = Color.FromArgb(90, 90, 90);
+            button5.BackColor = Color.FromArgb(90, 90, 90);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // set active color
+            button5.BackColor = Color.FromArgb(120, 120, 120);
+            button2.BackColor = Color.FromArgb(90, 90, 90);
+            button3.BackColor = Color.FromArgb(90, 90, 90);
+            button4.BackColor = Color.FromArgb(90, 90, 90);
+            button1.BackColor = Color.FromArgb(90, 90, 90);
+        }
+    }
+}
